2562ba8e3bb418b7397ca15bd4bd57beb070be2f
Jonathan Ikpeazu <jonathan.ikpeazu@gotinder.com>
2018-12-28 17:49:15 -0800

    initialize express app

---
+++ routes/rpc.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  return res.status(200); 
+});
+
+module.exports = router;
