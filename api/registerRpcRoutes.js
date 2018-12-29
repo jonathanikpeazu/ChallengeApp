2fb343779836d169719648890312b64108f32e3a
Jonathan Ikpeazu <jonathan.ikpeazu@gotinder.com>
2018-12-28 20:12:50 -0800

    comment

--- api/registerRpcRoutes.js
+++ api/registerRpcRoutes.js
@@ -1,6 +1,7 @@
 const { ChallengeController } = require('../controller');
 
 // todo - global Promise library
+// todo - dont send back mongo _v and other metadata
 module.exports = function registerRpcRoutes(rpcResponder) {
   rpcResponder.register('Challenge', {
     create: (params) => ChallengeController.create(params.attributes)
